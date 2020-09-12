c0b42ce56d41cbc3ffd6d1f9c4a8d18279d394e1
krantiprasad12 <57789517+krantiprasad12@users.noreply.github.com>
2020-09-12 17:25:44 +0530

    Updates

---
+++ precache-manifest.e40021c5cd6d70d1d9091f6f36a1b9c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750e81d6c0856e0a547e3284b75b6196",
+    "url": "/react-movie-search/index.html"
+  },
+  {
+    "revision": "fee5f8e3909d20ec603d",
+    "url": "/react-movie-search/static/js/2.d4c580ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-search/static/js/2.d4c580ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0375ce8b87fa145f0613",
+    "url": "/react-movie-search/static/js/main.942d9cb4.chunk.js"
+  },
+  {
+    "revision": "ec1afc3891ded9c1372a",
+    "url": "/react-movie-search/static/js/runtime-main.33682eb9.js"
+  }
+]);
